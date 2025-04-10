8457c59467220daaac01d1030bb53b98ebcfd54c
DaviGaleno <davigalenorocha@gmail.com>
2025-04-09 21:11:54 -0300

    Add files via upload

---
+++ exercicio 16.py
@@ -0,0 +1,12 @@
+amigos = ["Gabriel", "Maria", "Rafael", "Ana", "Lucas"]
+
+for amigo in amigos:
+    print(amigo)
+
+amigos = ["Gabriel", "Maria", "Rafael", "Ana", "Lucas"]
+
+i = 0
+
+while i < len(amigos):
+    print(amigos[i])
+    i += 1
